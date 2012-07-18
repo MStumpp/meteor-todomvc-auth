0a5915b11339a2d64ec8bd39b908cd99d42ed0e7
MStumpp <mstumpp@gmail.com>
2012-07-18 04:22:35 +0200

    some refactoring

--- accounts/server/secrets.js
+++ accounts/server/secrets.js
@@ -1,4 +1,4 @@
 // Modify and uncomment the following lines to configure login services.
 // Also see accounts/services.js
 
-Meteor.accounts.facebook.setSecret('e43c71494b79b619651a0a3c7ff2eddf');
+Meteor.accounts.facebook.setSecret('...');
